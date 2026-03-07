8a6355454df36b44a62b26b29659a150dcec7b3e
Sajeed191 <mohammed81sajeed@gmail.com>
2026-03-07 21:32:37 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,22 +1,25 @@
-from flask import Flask, request, jsonify
+from fastapi import FastAPI
+from pydantic import BaseModel
+
 from app.services.url_analyzer import analyze_url
 from app.services.keyword_detector import detect_keywords
 from app.services.risk_scorer import calculate_risk
 from app.predictor import predict
 from app.utils.validators import is_valid_url
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route("/analyze", methods=["POST"])
+class URLRequest(BaseModel):
+    url: str
 
-def analyze():
 
-    data = request.json
+@app.post("/analyze")
+def analyze(request: URLRequest):
 
-    url = data.get("url")
+    url = request.url
 
     if not is_valid_url(url):
-        return jsonify({"error": "Invalid URL"}), 400
+        return {"error": "Invalid URL"}
 
     features = analyze_url(url)
 
@@ -26,15 +29,10 @@ def analyze():
 
     risk_score = calculate_risk(features, keywords, ml_prediction)
 
-    return jsonify({
-
+    return {
         "url": url,
         "risk_score": risk_score,
         "keywords_found": keywords,
         "features": features,
         "ml_prediction": ml_prediction
-
-    })
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    }
