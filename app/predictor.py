7fb08e3f9bea0055ff809d8e848fecd4829c899e
Sajeed191 <mohammed81sajeed@gmail.com>
2026-03-07 16:45:24 +0530

    Update predictor.py

--- app/predictor.py
+++ app/predictor.py
@@ -1,14 +1,18 @@
 from app.feature_extractor import extract_features
 from app.model_loader import get_model
 
-def predict_url(url):
+model = get_model()
 
-    model = get_model()
+def predict_url(url):
 
     features = extract_features(url)
 
     prediction = model.predict([features])[0]
-
     probability = model.predict_proba([features])[0][1]
 
-    return prediction, probability
+    if prediction == 1:
+        result = "Phishing"
+    else:
+        result = "Safe"
+
+    return result, float(probability)
